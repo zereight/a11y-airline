539b435ba394c68f0281c4644cf8a72429cd13f5
zereight <fullstackmachinelearning@gmail.com>
2021-10-04 14:58:47 +0900

    feat: 더미 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useHistory } from "react-router";
+import Carousel from "./components/Carousel";
+import Nav from "./components/Nav";
 import SpinButton from "./components/SpinButton";
 
 const App = () => {
-  return <SpinButton />;
+  const history = useHistory();
+
+  return (
+    <>
+      <button type="button" onClick={() => history.push("/")}>
+        홈
+      </button>
+      <button type="button" onClick={() => history.push("/carousel")}>
+        캐로셀
+      </button>
+      <button type="button" onClick={() => history.push("/nav")}>
+        네비게이션
+      </button>
+      <Switch>
+        <Route exact path="/carousel" component={Carousel} />
+        <Route exact path="/nav" component={Nav} />
+        <Route exact path="/" component={SpinButton} />
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
